1d94c4c5e7ade64afe4088b659f1e5217f489717
Matheus Emidio de Melo Cadena <matheus8emidio@gmail.com>
2021-11-22 23:05:59 -0500

    Fixed API call function

---
+++ CryptoBalance/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CryptoBalance.Models
+{
+    public class Transaction
+    {
+        public string Price { get; set; }
+        public DateTime Time_stamp { get; set; }
+    }
+
+}
