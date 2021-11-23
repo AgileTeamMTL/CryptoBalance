94ebbf23e8686a2c169bef4d4e999855a0bb3312
Camilo Restrepo <camilorestrepo9411@gmail.com>
2021-11-22 23:07:13 -0500

    Dashboard updated

--- CryptoBalance/Controllers/HomeController.cs
+++ CryptoBalance/Controllers/HomeController.cs
@@ -26,5 +26,28 @@ namespace CryptoBalance.Controllers
 
             return View();
         }
+
+        public ActionResult Dashboard()
+        {
+            //Currency List
+            List<SelectListItem> itemsFiat = new List<SelectListItem>();
+            itemsFiat.Add(new SelectListItem { Text = "USD", Value = "0", Selected = true });
+            itemsFiat.Add(new SelectListItem { Text = "CAD", Value = "1" });
+            itemsFiat.Add(new SelectListItem { Text = "COP", Value = "2" });
+            itemsFiat.Add(new SelectListItem { Text = "BRL", Value = "3" });
+            ViewBag.FiatCurrencies = itemsFiat;
+
+            //Crypto currency coins
+            List<SelectListItem> itemsCrypto = new List<SelectListItem>();
+            itemsCrypto.Add(new SelectListItem { Text = "BTC", Value = "0", Selected = true });
+            itemsCrypto.Add(new SelectListItem { Text = "ETH", Value = "1" });
+            itemsCrypto.Add(new SelectListItem { Text = "ADA", Value = "2" });
+            itemsCrypto.Add(new SelectListItem { Text = "SOL", Value = "3" });
+            itemsCrypto.Add(new SelectListItem { Text = "CRO", Value = "4" });
+
+            ViewBag.CryptoCurrencies = itemsCrypto;
+
+            return View();
+        }
     }
 }
